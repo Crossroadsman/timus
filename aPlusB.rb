086e588dd9f3f83d33a1354180b3e3b66318b7a5
Crossroadsman <crossroadsman@gmail.com>
2015-03-27 13:23:38 -0600

    Create aPlusB.rb

---
+++ aPlusB.rb
@@ -0,0 +1,13 @@
+# a + b problem
+# Time limit: 1.0 second
+# Memory limit: 64 MB
+
+# Calculate a + b
+# Input: a and b
+# Output: a + b
+# Sample input: 1, 5
+# Sample output: 6
+
+def plus(a, b)
+  a + b
+end
